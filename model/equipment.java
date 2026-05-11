7874e2382619b9d2eb3293f104037dc32ead996e
Flower <gabrielmoita34@gmail.com>
2026-05-11 15:15:08 +0000

    developed more of the equipment and repair_request models

--- model/equipment.java
+++ model/equipment.java
@@ -1,12 +1,28 @@
 package model;
 
+import java.util.Date;
+
 public class equipment {
   private String responsible_user;
+  private String brand;
+  private String model;
+  private int SKU_code;
+  private Date manifacture_date;
+  private int lote;
 
   //will receave data from client
   //does that make sense? 
-  public equipment(String username){
+  public equipment(String username, String brand, String model, int SKU_code, Date manifacture_date, int lote){
     responsible_user = username;
+    this.brand = brand;
+    this.model = model;
+    this.SKU_code = SKU_code;
+    this.manifacture_date = manifacture_date;
+    this.lote = lote;
   }
 
+  public void create_equipment(){
+    //TODO: do questionair
+    //add to the database
+  }
 }
--- model/repair_request.java
+++ model/repair_request.java
@@ -1,19 +1,29 @@
 package model;
 
+import java.util.Date;
+
+import model.equipment;
+
 public class repair_request{
+  equipment equipment;
+
   private String responsible_user;
-  private boolean accepted;
+  private int SKU_code;
+  private int repair_code;
+  private Date submission_date;
+  private Date repair_date;
   private String assigned_Employee;
+  private boolean accepted;
 
-  public repair_request(String username){
+  public repair_request(String username, equipment equipment_to_request){
     responsible_user = username;
-    accepted = 0;
-
+    accepted = false;
+    
+    equipment = equipment_to_request;
   }
-  
+
   public void accept_request(String Employee_username){
     assigned_Employee = Employee_username;
-    accepted = 1;
-
+    accepted = true;
   }
 }
