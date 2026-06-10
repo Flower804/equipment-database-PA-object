7d8d4cf1cee9571e123c6eb4c0a76097ca709934
Flower <gabrielmoita34@gmail.com>
2026-06-10 16:50:17 +0000

    created documentation

--- controler/listing.java
+++ controler/listing.java
@@ -12,11 +12,27 @@ import model.repair_request;
 
 import database.SQLconnect;
 
+/**
+ *A class that contains all the listings possible to be done by the users to search inside the database
+ *@author Gabriel Moita
+ */ 
+
 public class listing{
+  
+  /**
+   *Calls the function responsible for listing all the users on the table "users"
+   *@param db the SQLconnect object thats being used to do the comunication bethween the database and the app
+   *@param input the Scanner object being used for inputs from the user
+   *@param type the type of the user that called the method 
+   */ 
   public void list_users(SQLconnect db, Scanner input, String type){
     list_users_classic(db, input, type);
   }
-
+  
+  /**Creates a filter to filter trough usernames
+   *
+   * @return the filter form of the leters that the user is searching for
+   */
   private String create_filter(){
     String filter;
     
@@ -26,7 +42,13 @@ public class listing{
     String final_filter = "%" + filter + "%";
     return final_filter;
   }
-
+  
+  /**The method accesable by managers that does the connection to the database to get a list of all the users existent, with possibilty of applied filter
+   *
+   *@param db the SQLconnect object thats being used to do the comunication bethween the database and the app
+   *@param input the Scanner object being used for inputs from the user
+   *@param type the type of the user that called the method 
+   */
   private void list_users_classic(SQLconnect db, Scanner input, String type){
     if(type.equals("manager")){
       boolean running = true;
--- model/Employee.java
+++ model/Employee.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
 import model.repair_request;
 import database.SQLconnect;
 
+/**
+ *The class that contains the methods for the "Employee" inheritance
+ *
+ *@author Gabriel Moita
+ */
 public class Employee extends User{
   int NIF;
   int phone_number;
@@ -18,26 +23,51 @@ public class Employee extends User{
 
   }
   
+  /**calls the method for the user Employee to change their own info
+   * 
+   * @param db the SQLconnect object thats being used to do the communication bethween the database and the app
+   * @param input the Scanner object being used for inputs from the user
+   */ 
   public void change_my_info(SQLconnect db, Scanner input){
     change_info(db, input);
   }
   
+  /**returns the current Employee's NIF number
+   *
+   *@return the Employee's NIF number 
+   */ 
   public int get_NIF(){
     return NIF;
   }
 
+  /**A method that gets the Employee's phone number
+   *
+   *@return the Employee's phone number
+   */ 
   public int get_phone_number(){
     return phone_number;
   }
-
+  
+  /**A method that gets the Employee's address
+   *
+   *@return the Employee's address
+   */ 
   public String get_address(){
     return address;
   }
-
+  
+  /**A method that gets the Employee's speciality
+   *
+   *@return the Employee's speciality
+   */ 
   public String get_speciality(){
     return speciality;
   }
 
+  /**A method that gets the date when the Employee started working
+   *
+   *@return the Employee's start date
+   */ 
   public String get_init_date(){
     return init_date;
   }
@@ -46,11 +76,21 @@ public class Employee extends User{
   private void view_current_requests(){
     ;
   }
-
+  
+  /**Accepts the request given to the Employee by the Manager
+   *
+   *@param repair_request the resquest made by the Client to be assigned to the Employee by the Manager
+   *@param Employee_username the username of the Employee to be assigned to the repair_request
+   */ 
   private void accept_request(repair_request repair_to_accept, String Employee_username){
     repair_to_accept.accept_request(Employee_username);
   }
-
+  
+  /**Changes the info of the current Employee
+   *
+   *@param db the SQLconnect object thats being used to do the comunication bethween the database and the app
+   *@param input the Scanner object being used for inputs from the user
+   */
   private void change_info(SQLconnect db, Scanner input){
     boolean running = true;
     int choice;
@@ -69,12 +109,21 @@ public class Employee extends User{
           db.change_my_info("users", "name", new_name, get_username());
           break;
         case(2):
+          //uhmmmm should we really do this?
+          //1- ask for username
+          //2- do verification to confirm not repeated
+          //3- change
 
           break;
         case(3):
+          System.out.println("What is the new password? ");
+          String new_password = input.nextLine();
+
+          db.change_my_info("users", "password", new_password, get_username());
 
           break;
         case(4):
+          System.out.println("Please ");
 
           break;
         case(5):
